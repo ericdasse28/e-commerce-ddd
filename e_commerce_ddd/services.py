34faa2e58b2afcdb2f8d22fb05c0ba3c925c2720
Eric Dasse <christ.gououi@gmail.com>
2023-12-01 18:16:33 +0100

    Add service layer

---
+++ e_commerce_ddd/services.py
@@ -0,0 +1,30 @@
+"""Service layer."""
+
+from sqlalchemy.orm import Session
+
+from e_commerce_ddd.domain import model
+from e_commerce_ddd.repository import AbstractRepository
+
+
+class InvalidSku(Exception):
+    pass
+
+
+def is_valid_sku(sku: str, batches: list[model.Batch]):
+    return sku in {b.sku for b in batches}
+
+
+def allocate(
+    line: model.OrderLine,
+    repo: AbstractRepository,
+    session: Session,
+) -> str:
+    batches = repo.list()
+
+    if not is_valid_sku(line.sku, batches):
+        raise InvalidSku(f"Invalid sku {line.sku}")
+
+    batchref = model.allocate(line, batches)
+
+    session.commit()
+    return batchref
