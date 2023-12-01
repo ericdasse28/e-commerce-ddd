f236c871ae3f4d8fbce5f1176fa20f133b7e9f47
Eric Dasse <christ.gououi@gmail.com>
2023-12-01 18:00:02 +0100

    Add e2e tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
+import time
+from pathlib import Path
+
 import pytest
-from sqlalchemy import create_engine
+import requests
+from requests.exceptions import ConnectionError
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import clear_mappers, sessionmaker
 
+from e_commerce_ddd import config
 from e_commerce_ddd.orm import mapper_registry, start_mappers
 
 
@@ -17,3 +24,89 @@ def session(in_memory_db):
     start_mappers()
     yield sessionmaker(bind=in_memory_db)()
     clear_mappers()
+
+
+def wait_for_postgres_to_come_up(engine):
+    deadline = time.time() + 10
+    while time.time() < deadline:
+        try:
+            return engine.connect()
+        except OperationalError:
+            time.sleep(0.5)
+
+    pytest.fail("Postgres never came up")
+
+
+def wait_for_webapp_to_come_up():
+    deadline = time.time() + 10
+    url = config.get_api_url()
+
+    while time.time() < deadline:
+        try:
+            return requests.get(url)
+        except ConnectionError:
+            time.sleep(0.5)
+
+    pytest.fail("API never came up")
+
+
+@pytest.fixture(scope="session")
+def postgres_db():
+    engine = create_engine(config.get_postgres_uri())
+    wait_for_postgres_to_come_up(engine)
+    mapper_registry.metadata.create_all(engine)
+
+    return engine
+
+
+@pytest.fixture
+def add_stock(postgres_session):
+    batches_added = set()
+    skus_added = set()
+
+    def _add_stock(lines):
+        for ref, sku, qty, eta in lines:
+            postgres_session.execute(
+                text(
+                    "INSERT INTO batches (reference, sku, _purchased_quantity,\
+ eta)"
+                    " VALUES (:ref, :sku, :qty, :eta)"
+                ),
+                {"ref": ref, "sku": sku, "qty": qty, "eta": eta},
+            )
+            [[batch_id]] = postgres_session.execute(
+                text(
+                    "SELECT id FROM batches WHERE reference=:ref AND sku=:sku",
+                ),
+                {"ref": ref, "sku": sku},
+            )
+
+            batches_added.add(batch_id)
+            skus_added.add(sku)
+
+        postgres_session.commit()
+
+    yield _add_stock
+
+    for batch_id in batches_added:
+        postgres_session.execute(
+            text("DELETE FROM allocations WHERE batch_id=:batch_id"),
+            {"batch_id": batch_id},
+        )
+
+        postgres_session.execute(
+            text("DELETE FROM batches WHERE id=:sku"), {"batch_id": batch_id}
+        )
+
+    for sku in skus_added:
+        postgres_session.execute(
+            text("DELETE FROM order_lines WHERE sku=:sku"), {"sku": sku}
+        )
+        postgres_session.commit()
+
+
+@pytest.fixture
+def restart_api():
+    (Path(__file__).parent.parent / "e_commerce_ddd/flask_app.py").touch()
+    time.sleep(0.5)
+    wait_for_webapp_to_come_up()
---
+++ tests/test_api.py
@@ -0,0 +1,50 @@
+"""Test the API."""
+
+import uuid
+
+import pytest
+import requests
+
+from e_commerce_ddd import config
+
+
+def random_suffix():
+    return uuid.uuid4().hex[:6]
+
+
+def random_sku(name=""):
+    return f"sku-{name}-{random_suffix()}"
+
+
+def random_batchref(name=""):
+    return f"batch-{name}-{random_suffix()}"
+
+
+def random_orderid(name=""):
+    return f"order-{name}-{random_suffix()}"
+
+
+@pytest.mark.usefixtures("restart_api")
+def test_allocations_are_persisted(add_stock):
+    sku = random_sku()
+    batch1, batch2 = random_batchref(1), random_batchref(2)
+    order1, order2 = random_orderid(1), random_orderid(2)
+    add_stock(
+        [
+            (batch1, sku, 10, "2011-01-01"),
+            (batch2, sku, 10, "2011-01-02"),
+        ]
+    )
+    line_1 = {"orderid": order1, "sku": sku, "qty": 10}
+    line_2 = {"orderid": order2, "sku": sku, "qty": 10}
+    url = config.get_api_url()
+
+    # First order uses up all stock in batch 1
+    response = requests.post(f"{url}/allocate", json=line_1)
+    assert response.status_code == 201
+    assert response.json()["batchref"] == batch1
+
+    # Second order should go to batch2
+    response = requests.post(f"{url}/allocate", json=line_2)
+    assert response.status_code == 201
+    assert response.json()["batchref"] == batch2
