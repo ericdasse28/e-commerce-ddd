dd4655a2a7fdd5f16e56f22674be49de23167cf7
Eric Dasse <christ.gououi@gmail.com>
2023-12-01 19:26:52 +0100

    Add e2e tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -48,3 +48,37 @@ def test_allocations_are_persisted(add_stock):
     response = requests.post(f"{url}/allocate", json=line_2)
     assert response.status_code == 201
     assert response.json()["batchref"] == batch2
+
+
+@pytest.mark.usefixtures("restart_api")
+def test_happy_path_returns_201_and_allocated_batch(add_stock):
+    sku, other_sku = random_sku(), random_sku("other")
+    early_batch = random_batchref(1)
+    later_batch = random_batchref(2)
+    other_batch = random_batchref(3)
+    add_stock(
+        [
+            (later_batch, sku, 100, "2011-01-02"),
+            (early_batch, sku, 100, "2011-01-01"),
+            (other_batch, other_sku, 100, None),
+        ]
+    )
+    data = {"orderid": random_orderid(), "sku": sku, "qty": 3}
+    url = config.get_api_url()
+
+    response = requests.post(f"{url}/allocate", json=data)
+
+    assert response.status_code == 201
+    assert response.json()["batchref"] == early_batch
+
+
+@pytest.mark.usefixtures("restart_api")
+def test_unhappy_path_returns_400_and_error_message():
+    unknown_sku, order_id = random_sku(), random_orderid()
+    data = {"orderid": order_id, "sku": unknown_sku, "qty": 20}
+    url = config.get_api_url()
+
+    response = requests.post(f"{url}/allocate", json=data)
+
+    assert response.status_code == 400
+    assert response.json()["message"] == f"Invalid sku {unknown_sku}"
